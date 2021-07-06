f38318626785a8c4fc1f4b358888f41d1612e151
Victor <victor3790@gmail.com>
2021-07-06 14:25:19 -0500

    Version changed to 3.0.0

--- tpc.php
+++ tpc.php
@@ -9,7 +9,7 @@
  * that starts the plugin.
  *
  * @link              victorcrespo.net
- * @since             1.0.0
+ * @since             3.0.0
  * @package           Tpc
  *
  * @wordpress-plugin
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'TPC_VERSION', '1.0.0' );
+define( 'TPC_VERSION', '3.0.0' );
 
 /**
  * plugin path
